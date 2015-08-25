56e4618bf253c40307af7cbcfb7c93e25650c30b
Christine Cazares <3cccazares@gmail.com>
2015-08-24 21:57:10 -0500

    Stared some of my projet

---
+++ Event.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace EventDatabase.Models
+{
+    public class Event
+    {    
+        public int EventId { get; set; }
+        public string Name { get; set; }
+        public string EventType { get; set; }
+        public DateTime EventDate { get; set; }
+        public decimal TotalBuget { get; set; }
+        public string Location { get; set; }
+        public decimal PriceOfLocation { get; set; }
+        public string EventAddressStreet { get; set; }
+        public  string EventAddressCity { get; set; }
+        public string Food { get; set; }
+        public decimal PriceOfFood { get; set; }
+        public string Entertainment { get; set; }
+        public string TypeOfEntertainment { get; set; }
+        public decimal PriceOfEntertainment { get; set; }
+        public string ColorOrThem { get; set; }
+        public decimal PriceOfDecortion { get; set; }
+        public decimal TotalPrice { get; set; }
+        public int NumberOfInviteScent { get; set; }
+        public int NumberOfConfirmed { get; set; }
+    }
+}
